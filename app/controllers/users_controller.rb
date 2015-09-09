7034aba05ce0862915e63e778c86d1c520efcda9
ji-no <mfm.ellegarden@gmail.com>
2015-09-09 18:11:06 +0900

    fix new buf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,10 @@ class UsersController < ApplicationController
   def new
     @new = User.new
     @user = User.all
-
   end
   def create
     @new = User.new(:name => params[:user][:name])
-    if @new[:name] != "" && !User.exists?(:name => @new[:name]) then
+    if @new[:name].strip != "" && !User.exists?(:name => @new[:name]) then
       @new.save
     end
     @user = User.all
