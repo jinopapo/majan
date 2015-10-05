c14d5edf8f152c7d69aca95274e6609fffd3fbae
ji-no <mfm.ellegarden@gmail.com>
2015-10-05 20:21:05 +0900

    add user_comp index.html

--- app/controllers/user_comp_controller.rb
+++ app/controllers/user_comp_controller.rb
@@ -3,27 +3,29 @@ class UserCompController < ApplicationController
     @user = User.all
     @map = {}
     @user.each do |var|
-      @map[var.name] = @map.fetch(var.name,[0])
+      @map[var.name] = {}
     end
     @user.each do |player|
+      player_score = Score.where("user_id = '#{player.id}'")
       @user.each do |enemy|
         if player.name == enemy.name then
-          @map[player.name].push(nil)
+          @map[player.name][enemy.name] = nil
         else
-          player_score = Score.where("user_id = '#{player.id}'")
           scores  = []
           player_score.each do |var|
+            comp_score = Score.find_by("user_id = '#{enemy.id}' and hand_id = '#{var.hand_id}'")
             if Score.find_by("user_id = '#{enemy.id}' and hand_id = '#{var.hand_id}'") then
               scores.push(var["score"])
             end
           end
           if scores.size == 0 then
-            @map[player.name].push(0)
+            @map[player.name][enemy.name] = 0
           else
-            @map[player.name].push(scores.inject(0.0){|r,i| r+=i }/scores.size)
+            @map[player.name][enemy.name] = scores.inject(0.0){|r,i| r+=i }/scores.size
           end
         end
       end
     end
+    p @map
   end
 end
