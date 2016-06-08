8e4b389c3ab6ac68cc29548cedc0a86a30cc85b7
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2016-06-08 17:45:04 +0900

    rm old code

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -8,26 +8,8 @@ class ResultController < ApplicationController
   end
 
   def graph
-    d = Date.today
-    if d.month < 4
-      queries = "created_at > '#{d.year-1}-04-01'"
-    else
-      queries = "created_at > '#{d.year}-04-01'"
-    end
-    @hands = Hand.where(queries)
-    @users = User.all
   end
 
   def rank
-    d = Date.today
-    if d.month < 4
-      queries = "created_at > '#{d.year-1}-04-01'"
-    else
-      queries = "created_at > '#{d.year}-04-01'"
-    end
-    @score_sum = Score.where(queries).order("sum_score DESC").group(:user_id).sum(:score)
-    @score_ave = Score.where(queries).order("average_score DESC").group(:user_id).average(:score)
-    @rank_ave = Score.where(queries).order("average_rank").group(:user_id).average(:rank)
-    @users = User.all
   end
 end
